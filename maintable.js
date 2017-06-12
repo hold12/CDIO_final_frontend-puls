e9ef38120357ec42bc8cb74afa99cf30437f7250
Evilwerner <kristopuls@gmail.com>
2017-06-13 01:18:38 +0200

    User Administration can use JSON object to make table

--- maintable.js
+++ maintable.js
@@ -1,14 +1,26 @@
 
 function getUsers(jsonobject) {
-    for (i = 0; i < Object.keys(jsonobject).length; i++) {
-        $("#table-row").append("<tr>");
+  $("#table-column").append("<tr>");
+  $("#table-column").append("<td>UserID</td>");
+  $("#table-column").append("<td>firstname</td>");
+  $("#table-column").append("<td>lastname</td>");
+  $("#table-column").append("<td>Initials</td>");
+  $("#table-column").append("<td>Password</td>");
+  $("#table-column").append("<td>Roles</td>");
+  $("#table-column").append("</tr>");
+      for (i = 0; i < Object.keys(jsonobject).length; i++) {
         console.log(jsonobject[0]);
-        $("#table-column").append("<td>" + jsonobject[i].userId + "</td>");
-        $("#table-column").append("<td>" + jsonobject[i].firstname + "</td>");
-        $("#table-column").append("<td>" + jsonobject[i].lastname + "</td>");
-        $("#table-column").append("<td>" + jsonobject[i].initials + "</td>");
-        $("#table-column").append("<td>" + jsonobject[i].password + "</td>");
-        // $("#table-column").append("<td>" + jsonobject[i].roles + "</td>");
-        $("#table-row").append("</tr>");
+        $("#table-column").append("<tr class=\"active\">");
+        $("#table-column").append("<td>"+jsonobject[i].userId+"</td");
+        $("#table-column").append("<td>"+jsonobject[i].firstname+"</td");
+        $("#table-column").append("<td>"+jsonobject[i].lastname+"</td");
+        $("#table-column").append("<td>"+jsonobject[i].initials+"</td");
+        $("#table-column").append("<td>"+jsonobject[i].password+"</td");
+        $("#table-column").append("<td>" + jsonobject[i].roles.role_name + "</td>");
+        $("#table-column").append("</tr>");
+
       }
+      // $("#table-end").append("</table>");
+
+
 };
